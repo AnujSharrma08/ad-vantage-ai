b0bcf586aaf77744a845b2fa854693a6ba415fb3
anuj_sharrma <sharrma26@gmail.com>
2026-01-26 15:00:23 +0530

    ip again update

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const baseURL = 'http://13.60.80.190/api/v1/';
-// export const baseURL = 'https://lady-watched-greatly-combinations.trycloudflare.com/api/v1/';
+// export const baseURL = 'http://13.60.80.190/api/v1/';
+export const baseURL = 'https://budgets-powerpoint-decorating-unlike.trycloudflare.com/api/v1/';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
