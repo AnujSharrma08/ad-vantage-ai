aea6686f92b7a14b1b6a2f1413fb451e5bc1b2e5
anuj_sharrma <sharrma26@gmail.com>
2026-03-12 12:11:08 +0530

    ip updated

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const baseURL = 'http://13.53.130.236/api/v1/';
-export const baseURL = ' https://eyes-posts-touched-correction.trycloudflare.com/api/v1/';
+export const baseURL = ' https://starting-insertion-reviewer-coverage.trycloudflare.com/api/v1/';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
